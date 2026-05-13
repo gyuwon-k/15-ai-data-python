2011fe42daaa74a8cbf0363ea7480998465d36d0
gyuwon-k <gyuwon2202@gmail.com>
2026-05-13 10:30:18 +0900

    처음 세팅 0513

---
+++ 사전교육/04_파이썬객체/mod1.py
@@ -0,0 +1,10 @@
+def add(a,b):
+	return a+b
+
+def sub(a,b):
+	return a-b
+
+#print(__name__) import를 사용하지 않을 때 사용함
+if __name__ == "__main__":
+	print(add(100,200))
+	print(sub(500,100))
---
+++ 사전교육/imsi/test2.py
@@ -0,0 +1,1 @@
+print("life is too short you need python")
